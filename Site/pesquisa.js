284149505b0bb725714b3c9fb556ee62aff7361e
Gabriel Camelo <ganriel201@gmail.com>
2019-06-26 11:42:56 -0300

    correçoes na funçao

--- Site/pesquisa.js
+++ Site/pesquisa.js
@@ -1,9 +1,9 @@
-var inputElement = document.getElementById("pesquisa");
-var pesq = inputElement.innerText;
-
-var btnElement = document.getElementById("botao");
+var btnElement = document.querySelector("#botao");
 
 btnElement.onclick = function() {
-    alert(pesq)
-    window.location = "https://g1.globo.com/busca/?q=" + pesq;
+    var inputElement = document.querySelector("#pesquisa");
+    var pesq = inputElement.value;
+
+    alert(pesq);
+    window.open("https://g1.globo.com/busca/?q=" + pesq);
 }
